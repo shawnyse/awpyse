163be5f41ec87f975c97839c023be9e8a9c4a465
shawnyse <shawnyse625@gmail.com>
2020-01-09 04:16:02 +0800

    Added routing configuration of commentcontroller

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -11,5 +11,7 @@ Route::group([
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index')->name('admin.home');
+    $router->resource('users', UserController::class);
+    $router->resource('users', CommentController::class);
 
 });
